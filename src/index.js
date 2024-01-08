9929b0158ea57ce5a6c671f819b6943d4d8bb59a
Yash4523 <147232696+Yash4523@users.noreply.github.com>
2024-01-08 15:22:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './component/Home';
-ReactDOM.render(<Home/>, document.getElementById('root'));
+// ReactDOM.render(<Home/>, document.getElementById('root'));
+const root = React.createRoot(document.getElementById('root'));
+root.render( <Home />);
 
 
